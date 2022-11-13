2c25c17e5179aa18d4f7ce2cdc3967f0dc488f4b
dandan002 <danieljung578@gmail.com>
2022-11-12 23:23:14 -0500

    dfs

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
-app.use(express.static('images'));
+app.use(express.static("images"));
 app.get('/style.css', function(req, res) {
   res.sendFile(__dirname + "/" + "style.css");
 });
 const path = require('path');
-const router = express.Router();
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/upload',function(req,res){
+app.get('/upload.html',function(req,res){
   res.sendFile(path.join(__dirname+'/upload.html'));
 });
 
-//add the router
-app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
