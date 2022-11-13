07c5d76002ae07f4378ca1fbdd5303e9dff62175
dandan002 <59246676+dandan002@users.noreply.github.com>
2022-11-12 21:43:08 -0500

    d

--- daniel.js
+++ daniel.js
@@ -1,3 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+
 let latlon = "";
 			const successCallback = (position) => {
 				console.log(position);
